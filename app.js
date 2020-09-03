44db714cc8bac9edd92748d5c242453b72a1603c
oluwatobi john <oluwatobijohn49@gmail.com>
2020-09-03 15:04:19 +0100

    Append list Input

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ function addTask(e) {
     }
 
 
+    //List item with delete button
+//Create li element
+const li = document.createElement('li');
+//add class
+li.className = 'collection-item'
+//create text node
+li.appendChild(document.createTextNode(taskInput.value));
+//create new link element
+const link = document.createElement('a');
+//add class
+link.className = 'delete-item secondary-content';
+//add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+//append the link to li
+li.appendChild(link);
+
+
+//Append li to ul
+taskList.appendChild(li);
 
 //clear input
 taskInput.value = '';
